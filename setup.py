8b2e0136ce969ccd7fe7cb97422fe4256e2498c4
Bochoxic <s222928@dtu.dk>
2023-11-07 10:52:22 +0100

    First commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Implementation of Sucessive Refinement of Images with Deep Joint Source-Channel Coding',
+    author='Group 30',
+    license='',
+)
